1b465f8fb2a902684c29af5c9169c1839f3615ce
bysmartos <bysmartos@gmail.com>
2022-08-26 01:12:24 +0200

    all functionality done and style, refactorization NO done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
 before_action :authenticate_user!, except: %i[show index]
 
   def index
-    @articles = Article.all
+    @articles = Article.all.order(created_at: :desc)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,13 @@
   # Defines the root path route ("/")
   # root "articles#index"
   Rails.application.routes.draw do
+    get 'users/profile'
     devise_for :users, controllers: {
       sessions: 'users/sessions',
       registrations: 'users/registrations',
     }
+
+    get '/u/:id', to: 'users#profile', as: 'user'
 
     get 'about', to: 'pages#about'
     root "pages#home"
